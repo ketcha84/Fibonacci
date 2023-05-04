5bddd7ff8b061280f36857ac5ecfee88e85481c6
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-04 19:15:33 +0200

    added digit filter

--- src/Run.java
+++ src/Run.java
@@ -1,9 +1,11 @@
 import controller.FibNums;
+import gui.GuiFib;
 
 import java.util.Arrays;
 
 public class Run {
     public static void main(String[] args) {
+        GuiFib gui= new GuiFib();
         long[] fib = FibNums.fibNums(10);
         System.out.println(Arrays.toString(fib));
     }
--- src/gui/GuiFib.java
+++ src/gui/GuiFib.java
@@ -1,4 +1,61 @@
 package gui;
 
-public class GuiFib {
+import javax.swing.*;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.PlainDocument;
+import java.awt.*;
+
+public class GuiFib extends JFrame {
+    private JPanel rootPanel;
+    private JLabel lbl_1;
+    private JTextField txtNum;
+    private JButton btn_gen;
+    private JTextArea generatedNubersFibonacciTextArea;
+    private JScrollBar horScroll;
+    private JScrollBar vertScroll;
+
+    public GuiFib() throws HeadlessException {
+        init();
+    }
+
+    private void init() {
+        add(rootPanel);
+        setTitle("Fibonacci Numbers.");
+        setBounds(0, 0, 400, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        //init txt
+        PlainDocument docNum = (PlainDocument) txtNum.getDocument();
+        docNum.setDocumentFilter(new DigitFilter());
+    }
+
+
+    class DigitFilter extends DocumentFilter {
+        public static final String DECIMAL_DIGITS = "\\d+";
+
+        private String regEx;
+
+        public DigitFilter() {
+            this.regEx = DECIMAL_DIGITS;
+        }
+
+        @Override
+        public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+
+            if (string.matches(regEx)) {
+                super.insertString(fb, offset, string, attr);
+            }
+        }
+
+        @Override
+        public void replace(FilterBypass fb, int offset, int length, String string, AttributeSet attrs) throws BadLocationException {
+            if (string.matches(regEx)) {
+                super.replace(fb, offset, length, string, attrs);
+            }
+        }
+    }
 }
