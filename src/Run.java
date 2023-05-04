d28e518e8615f3e026c5b68d63c986ab2a740052
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-04 19:32:59 +0200

    created logics

--- src/Run.java
+++ src/Run.java
@@ -6,7 +6,5 @@ import java.util.Arrays;
 public class Run {
     public static void main(String[] args) {
         GuiFib gui= new GuiFib();
-        long[] fib = FibNums.fibNums(10);
-        System.out.println(Arrays.toString(fib));
     }
 }
--- src/gui/GuiFib.java
+++ src/gui/GuiFib.java
@@ -1,29 +1,62 @@
 package gui;
 
+import controller.FibNums;
+
 import javax.swing.*;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
 import javax.swing.text.PlainDocument;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class GuiFib extends JFrame {
     private JPanel rootPanel;
     private JLabel lbl_1;
     private JTextField txtNum;
     private JButton btn_gen;
-    private JTextArea generatedNubersFibonacciTextArea;
+    private JTextArea txtGenNums;
     private JScrollBar horScroll;
     private JScrollBar vertScroll;
 
     public GuiFib() throws HeadlessException {
         init();
+        initBtn();
+    }
+
+    private void initBtn() {
+        btn_gen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               generateSequence();
+            }
+        });
+    }
+
+    private void generateSequence() {
+        long[] arr = FibNums.fibNums(Integer.valueOf(txtNum.getText()));
+        StringBuilder builder = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            builder.append(arr[i] + "; ");
+            if (++count == 15) {
+                builder.append("\n");
+                count = 0;
+            }
+        }
+        builder.append(arr[arr.length - 1]);
+        txtGenNums.setText(builder.toString());
+        builder = null;
+        arr = null;
     }
 
     private void init() {
         add(rootPanel);
         setTitle("Fibonacci Numbers.");
         setBounds(0, 0, 400, 300);
+        setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
