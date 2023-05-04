b6e3529eca875142b88f282a5816cdd1e5afbab9
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-04 18:49:03 +0200

    added Run class

--- src/Run.java
+++ src/Run.java
@@ -1,5 +1,10 @@
+import controller.FibNums;
+
+import java.util.Arrays;
+
 public class Run {
     public static void main(String[] args) {
-
+        long[] fib = FibNums.fibNums(10);
+        System.out.println(Arrays.toString(fib));
     }
 }
--- src/controller/FibNums.java
+++ src/controller/FibNums.java
@@ -1,4 +1,14 @@
 package controller;
 
 public class FibNums {
+    public static long[] fibNums(int num) {
+        long[] arr = new long[num + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i <= num; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr;
+    }
 }
